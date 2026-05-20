1a0e88affa1d23e1a91530315b97fe732a06de80
Pierre Riblet Cahurel <pierre.ribletcahurel@etudiant.univ-rennes.fr>
2026-05-20 18:10:33 +0200

    constructeur discussion

--- src/main/java/messagerie_instantane/server/Discussion.java
+++ src/main/java/messagerie_instantane/server/Discussion.java
@@ -1,10 +1,19 @@
 package messagerie_instantane.server;
 
-
+import java.util.List;
 import java.rmi.RemoteException;
 import messagerie_instantane.interfaces.*;
+import messagerie_instantane.client.*;
 
 public class Discussion implements InterfaceSujetDiscussion{
+    List<Client> participants;
+    String nom;
+
+    public Discussion(String nom) throws RemoteException{
+        this.nom = nom;
+        // Récupérer la liste des participants.
+    }
+
     /**
      * 
      * @param c
@@ -14,10 +23,8 @@ public class Discussion implements InterfaceSujetDiscussion{
      */
     @Override
     public void inscription(InterfaceAffichageClient c) throws RemoteException {
-        // I. Récupérer données utilisateurs
-        // II. Effectuer diverses vérifications sur ces données, avec renvoi d'erreur si problème
-        // III. Si pas d'erreur, envoie d'une requête au DAO.
-        // IV. Affichage d'un message via c.affiche() en fonction de la réponse du DA0 : Réussite de l'inscription ou erreur quelconque.
+        // I. Récupérer un objet client depuis le DAO.
+        // II. L'ajouter à la liste des participants.
         throw new UnsupportedOperationException("Unimplemented method 'inscription'");
     }
 
@@ -29,7 +36,7 @@ public class Discussion implements InterfaceSujetDiscussion{
      */
     @Override
     public void desInscription(InterfaceAffichageClient c) throws RemoteException {
-        // I. Effectuer une requête au DAO, probablement avec l'id de l'utilisateur, pour supprimer les données lui correspondant dans la BBD.
+        // I. Effectuer une requête au DAO avec l'id de l'utilisateur, pour supprimer les données lui correspondant dans la BBD.
         // II. Selon le retour du DAO, affichage d'un message via c.affiche() en fonction de la réponse du DA0 : Réussite de la désinscription ou erreur quelconque.
         throw new UnsupportedOperationException("Unimplemented method 'desInscription'");
     }
@@ -42,9 +49,11 @@ public class Discussion implements InterfaceSujetDiscussion{
      */
     @Override
     public void diffuse(String Message) throws RemoteException {
-        // I. Stocker le message en BDD.
-        // II. Diffuser le message à tous les clients.
-        throw new UnsupportedOperationException("Unimplemented method 'diffuse'");
+        // I. Stocker le message en BDD en envoyant un message DAO.
+        // II. Diffuser le message à tous les clients avec affiche pour chaque client.
+        
+        for (Client p : participants) {
+            p.affiche(Message);
+        }
     }
-    
 }
