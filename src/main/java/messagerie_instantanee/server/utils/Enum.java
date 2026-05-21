fc3af3f936a501fca43e7e04c53c15ef32eb8461
Maodan DESBLE <118923564+gomenasia@users.noreply.github.com>
2026-05-21 12:03:06 +0200

    squellette enum

---
+++ src/main/java/messagerie_instantanee/server/utils/Enum.java
@@ -0,0 +1,20 @@
+package messagerie_instantanee.server.utils;
+
+public class Enum {
+    
+    public enum Role {
+        Admin, Gueux;
+
+        /**
+        * Génère le string de la colonne SQL adapter à la requête
+        * 
+        * @return String de la colonne SQL correspondant à la granularité sélectionnée
+        */
+        public String getColonneSQL() {
+            return switch (this) {
+                case Admin -> "Admin";
+                case Gueux -> "Gueux";
+            };
+        }
+    }
+}
