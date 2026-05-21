55d7bdc690af3e9f106876ed4324db34dade35bc
Pierre Riblet Cahurel <pierre.ribletcahurel@etudiant.univ-rennes.fr>
2026-05-21 17:35:51 +0200

    a

--- src/main/java/messagerie_instantanee/interfaces/InterfaceSujetDiscussion.java
+++ src/main/java/messagerie_instantanee/interfaces/InterfaceSujetDiscussion.java
@@ -7,5 +7,5 @@ import messagerie_instantanee.server.models.*;
 public interface InterfaceSujetDiscussion extends Remote {
     public void inscription(InterfaceAffichageClient c, User user) throws RemoteException;
     public void desInscription(InterfaceAffichageClient c, User user) throws RemoteException;
-    public void diffuse(String Message) throws RemoteException;
+    public void diffuse(Message Message) throws RemoteException;
 }
