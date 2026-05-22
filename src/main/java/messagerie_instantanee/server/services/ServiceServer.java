e549fca80df652db121dd02562ba741e5d66a218
Maodan DESBLE <118923564+gomenasia@users.noreply.github.com>
2026-05-22 09:23:45 +0200

    fix surement de struc interessant

--- src/main/java/messagerie_instantanee/server/services/ServiceServer.java
+++ src/main/java/messagerie_instantanee/server/services/ServiceServer.java
@@ -11,7 +11,7 @@ public class ServiceServer {
     public static List<Discussion> salonToDiscussion(Collection<Salon> salons){
         List<Discussion> lst_discussions = new ArrayList<>();
         for(Salon salon : salons){
-            lst_discussions.add(new Discussion(salon.getSalonId(), salon.getSalonNom(), null));
+            lst_discussions.add(new Discussion(salon.getSalonId(), salon.getSalonNom(), null, false));
         }
         return lst_discussions;
     }
