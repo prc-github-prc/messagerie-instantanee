ed7eaece860e75c05ad13ccbef1975ef6e39bd73
Maodan DESBLE <118923564+gomenasia@users.noreply.github.com>
2026-05-22 11:57:12 +0200

    suite cast server

--- src/main/java/messagerie_instantanee/interfaces/InterfaceServeurForum.java
+++ src/main/java/messagerie_instantanee/interfaces/InterfaceServeurForum.java
@@ -2,16 +2,13 @@ package messagerie_instantanee.interfaces;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
+
+import messagerie_instantanee.server.models.Discussion;
 
 public interface InterfaceServeurForum extends Remote {
-    /**
-     * 
-     * @param titre
-     * @return
-     * @throws RemoteException
-     * 
-     * Obtient un salon à partir d'un titre de discussion.
-     */
-    public InterfaceSujetDiscussion obtientSujet(String titre) throws RemoteException;
+    InterfaceSujetDiscussion obtientSujet(String titre) throws RemoteException;
+    Boolean checkId(String pseudo, String pwd_hash) throws RemoteException;
+    List<Discussion> listerSalons() throws RemoteException;
+    Boolean creationUser(String username, String hash) throws RemoteException;
 }
-
