48448050a63fdd5dfc6a92aeecf67d631efd063f
noalefeuvre <noam.lefeuvre@etudiant.univ-rennes.fr>
2026-05-24 00:25:20 +0200

    FEAT : CLOSE server + fix discussionDAO

--- src/main/java/messagerie_instantanee/server/Server.java
+++ src/main/java/messagerie_instantanee/server/Server.java
@@ -17,6 +17,7 @@ import messagerie_instantanee.server.database.DAO.DiscussionDAO;
 import static messagerie_instantanee.server.database.DAO.UserDAO.findUserByUsername;
 import static messagerie_instantanee.server.database.DAO.UserDAO.insertUser;
 import static messagerie_instantanee.server.services.ServiceServer.salonToDiscussion;
+import messagerie_instantanee.server.database.DAO.DiscussionDAO.updateDiscussion;
 
 import messagerie_instantanee.server.database.DatabaseLaucher;
 import messagerie_instantanee.server.models.Discussion;
@@ -87,6 +88,11 @@ public class Server extends UnicastRemoteObject implements InterfaceServeurForum
      * serveur.close gère le stockage en BDD des données courantes.
      */
     public void close() {
-        // TODO finish me
+        try {
+            map_salons.values().stream()
+                .forEach((Salon salon) -> DiscussionDAO.updateDiscussion(salon.getSalonNom(), salon.getEstPrivee(), salon.getAdmin(), salon.getUser()));
+        } catch (SQLException e) {
+            System.out.println("[Server] Erreur lors de la fermeture du serveur : " + e.getMessage());
+        }
     }
 }
